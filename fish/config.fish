f887f815554c5ea229546a2137bba0ab1a538231
Your Name <briggsbastianp@gmail.com>
2022-01-14 11:25:40 -0800

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+
+set fish_greeting ""
+
+alias ls "exa"
+alias lla "ls -lla"
+alias editFish "nvim ~/.config/fish/config.fish"
+alias refreshFish "source ~/.config/fish/config.fish";
+alias vim "nvim"
+alias balena "~/Tools/Balena/balenaEtcher-1.7.3-x64.AppImage"
+alias VCVR "cd /home/briggs/Tools/VCV_RACK/Rack2Free/ && ./Rack"
+alias edit "nvim"
+alias r "ranger"
+alias g "git"
